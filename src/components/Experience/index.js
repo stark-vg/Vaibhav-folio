849a017be52fefc8624bdb262010ea1127e4db3a
Vaibhav Goel <vaibhavgoel1904@gmail.com>
2023-10-06 05:30:58 +0530

    2nd

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                    My work experience as a software engineer and working on different companies and projects. My total work experience is of 20months. Basically i started working as Trainee Engineer in Trangile Pvt. Ltd. for 8+ months as a full stack developer.    
                 </Desc>
                 <TimelineSection>
                     <Timeline>
